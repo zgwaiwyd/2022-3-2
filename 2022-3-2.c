1da5de0dd391c30b345be039a2a34cf2ad2d305d
zgwaiwyd <99521061+zgwaiwyd@users.noreply.github.com>
2022-03-02 23:11:18 +0800

    Add files via upload

---
+++ 2022-3-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+printf("hello,world!");
+}
+
+
+//int max(int a,int b)
+//{
+//	if (a<b)
+//		return a;
+//	else
+//		return b;
+//}
+//void main ()
+//{
+//	int max(int a ,int b);
+//	int x,y,z;
+//	printf("input two numbers:\n");
+//	scanf("%d%d",&x,&y);
+//	z=max(x,y);
+//	printf("maxmum=%d",z);
+//	printf("youwenti");
+//	return 0;
+//}
+
+//int main()
+//
+//{
+//
+//	return 0;
+//}
